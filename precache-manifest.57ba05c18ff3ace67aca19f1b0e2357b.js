9924f5013fcd2d610ec0eb41441b4737bb9453f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 16:08:42 +0000

    Deploy QuiErro/QuiBoard to github.com/QuiErro/QuiBoard.git:gh-pages

---
+++ precache-manifest.57ba05c18ff3ace67aca19f1b0e2357b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485dcdfcaf110c55a43fb71cb324cb00",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3a6a81b3f30493864cdf.bundle.js"
+  },
+  {
+    "url": "runtime~main.3a6a81b3f30493864cdf.bundle.js"
+  },
+  {
+    "revision": "56230b7cfc0efe66f7e3",
+    "url": "static/css/main.c8d314b8.chunk.css"
+  },
+  {
+    "revision": "b0880d7bfebf50080aee",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.3a6a81b3f30493864cdf.bundle.js"
+  },
+  {
+    "url": "vendors~main.3a6a81b3f30493864cdf.bundle.js.LICENSE.txt"
+  }
+]);
